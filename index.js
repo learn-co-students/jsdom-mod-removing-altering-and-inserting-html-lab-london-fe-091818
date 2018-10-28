48760347353fed0b0871b6e88820a2535ead628e
Kat Maskell <vampirebatkat@gmail.com>
2018-10-28 12:43:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 /* Enter the code to remove the main node element under this comment */
 
+// .remove('main')
+// getElementById('main').remove()
+console.log(document.getElementById('main'))
+document.getElementById('main').remove()
+
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerText = 'Kat is the champion!';
+document.body.appendChild(newHeader);
