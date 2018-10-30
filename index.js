7ee9fbe2f02482264c8e128bb8e26a838fffe6fb
Gabriele Sartoris <sartoris.gabriele@gmail.com>
2018-10-30 19:30:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-/* Enter the code to remove the main node element under this comment */
+main.remove();
 
-/* Create your new element here and assign it to newHeader */
+var newHeader = document.createElement('h1#victory');
+
 const newHeader = null;
