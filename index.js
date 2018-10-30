e7a873d128b02cb0ca462e52b04feeefa7f7d1d9
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-30 20:21:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 /* Enter the code to remove the main node element under this comment */
+ document.querySelector("main#main").remove();
+
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+// const newHeader = null;
+
+const newHeader = document.createElement("h1");
+newHeader.innerText = "Steven is the champion!";
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+
